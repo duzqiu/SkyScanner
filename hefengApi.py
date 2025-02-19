94fff2a2e45252ce28d080cd8c0db77d79156538
duzqiu <35525043+duzqiu@users.noreply.github.com>
2025-02-19 17:11:06 +0800

    更新 hefengApi.py

--- hefengApi.py
+++ hefengApi.py
@@ -37,15 +37,15 @@ show_time = update_time.split("+")[0].replace("T", " ")
 title = f"{show_time}"
 test = "/"
 content = f"""
-地点: 上海-浦东新区
+✅地点: 上海-浦东新区
 
-天气: {text}；温度: {temp}°C；体感温度: {feels_like}°C
+✅天气: {text}；温度: {temp}°C；体感温度: {feels_like}°C
 
-风向: {wind_dir}；风力: {wind_scale}级；风速: {wind_speed}km
+✅风向: {wind_dir}；风力: {wind_scale}级；风速: {wind_speed}km
 
-湿度: {humidity}%；降水量: {precip}mm
+✅湿度: {humidity}%；降水量: {precip}mm
 
-信息来源: 和风天气api
+❎信息来源: 和风天气api
 """
 bark = SendBark(key)
 bark.send_t_c(title, content)
